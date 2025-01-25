c3da36256bf7e2ae102e8cc99e42a1cfb6ae8230
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2025-01-25 06:28:06 +0000

    initial work

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,30 @@
 import fs from "node:fs";
+import path from "node:path";
+import yargs from "yargs";
 import postcssFunctions from "postcss-functions";
 import postcssSass from "@csstools/postcss-sass";
 import { selectorReplacerPlugin } from "steam-theming-utils/postcss-plugin";
 
-const result = fs.readFileSync("steam_version").toString();
-const getSteamVersion = () => `"${result}"`;
+// Generate an index.css file that imports everything
+const { argv } = yargs(process.argv);
+const text = fs
+	.readdirSync(argv.base, { recursive: true })
+	.filter((e) => e.endsWith(".scss"))
+	.map((e) => `@import "${e.replace("scss", "css")}";`)
+	.join("\n");
+fs.writeFileSync(path.join(argv.dir, "index.css"), text);
+
+/**
+ * `icon("name")` => `url("data:image/png;base64,${base64}")`
+ *
+ * @param {string} name File name without the extension.
+ */
+function icon(name) {
+	const file = path.join("assets", `${name.replace(/"/g, "")}.svg`);
+	const base64 = fs.readFileSync(file, { encoding: "base64" });
+
+	return `url("data:image/png;base64,${base64}")`;
+}
 
 /** @type {import("postcss-load-config").Config} */
 export default {
@@ -16,7 +36,7 @@ export default {
 		}),
 		postcssFunctions({
 			functions: {
-				"steam-version": getSteamVersion,
+				icon,
 			},
 		}),
 	],
